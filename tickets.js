ec27b26889cdf1dd90c41bbffe1a63ea7fdaa8c9
Muhammad Rameez <32459297+MRameez820@users.noreply.github.com>
2023-02-21 11:50:37 +0500

    Update tickets.js

--- tickets.js
+++ tickets.js
@@ -118,6 +118,7 @@ $('#page-section').show()
 }
 
 $('#send-otp').click(function(){
+console.log(addobj )	
   $('#otp_error').hide()
     $('#num_error').hide()
 phonenum = $('#phone').val()
@@ -230,30 +231,3 @@ function updateapi(rowid,old_status)
        });
 	
 	}
-
-
-/*
-function setCookie(name,value,days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
-}
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
-}
-
-function eraseCookie(name) {   
-    document.cookie = name+'=; Max-Age=-99999999;';  
-}
-*/
